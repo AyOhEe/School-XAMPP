5d959f9cc005a0cc6892350052acfd989bc4fdff
AyOhEe <74308087+AyOhEe@users.noreply.github.com>
2023-09-06 22:42:52 +0100

    Removed XAMPP Dashboard navtarget
    It doesn't need to be there, as pretty as it is.

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,11 +1,28 @@
-<?php
-	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
-		$uri = 'https://';
-	} else {
-		$uri = 'http://';
-	}
-	$uri .= $_SERVER['HTTP_HOST'];
-	header('Location: '.$uri.'/navigation/');
-	exit;
-?>
-Something is wrong with the XAMPP installation :-(
+<html>
+    <body>
+        <main>
+            <?php echo "<h1>Navigation - htdocs</h1>"?>
+            <?php echo "Today is ".date("Y/m/d")?>
+            <div id="clock"> It is currently 00:00:00 on a Monday </div>
+        
+            <ul><?php
+                $parentdir = "navtargets";
+                $directories = glob($parentdir . '/*' , GLOB_ONLYDIR);
+                foreach ($directories as $dir){
+                    echo "<li>";
+                    echo "<a href =\"".$dir."\">";
+                    echo substr($dir, strlen($parentdir) + 1);
+                    echo "</a>";
+                    echo "</li>";
+                }
+            ?></ul>
+        </main>
+    </body>
+
+
+    
+    <head>
+        <link href="styles.css" rel="stylesheet" type="text/css">
+        <script src="p-time.js"></script>
+    </head>
+</html>
