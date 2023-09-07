8f024ef0b3e831161c477224b74399216badb588
AyOhEe <74308087+AyOhEe@users.noreply.github.com>
2023-09-07 21:16:32 +0100

    Jokes website can now search by keyword

--- htdocs/navtargets/Jokes Website/index.php
+++ htdocs/navtargets/Jokes Website/index.php
@@ -5,25 +5,31 @@
 
     <body>
         <h1>Jokes page</h1>
-        <a href="/">Back to navigation</a>
+        <p>
+            <a href="/">Back to navigation</a>
+        </p>
 
         <?php
 
-        
-        include "db.php";
-        /*//show error if connection failed. otherwise show connection info
+        /*//include "db.php";
+        //show error if connection failed. otherwise show connection info
         if ($mysqli->connect_errno){
             echo "Failed to connect to MySQL";
         }
-        echo $mysqli->host_info ."<br>";*/
+        echo $mysqli->host_info ."<br>";
 
-
-        include "search_all_jokes.php";
-        include "search_keyword.php";
+        //include "search_all_jokes.php";
 
         //close the connection: we're done here
-        $mysqli->close();
-        
+        //$mysqli->close();
+        */
         ?>
+
+        <form action="search_keyword.php">
+            Please enter a keyword to search for:<br>
+            <input type="text" name="keyword" value="Chicken"><br>
+
+            <input type="submit" value="Submit">
+        </form>
     </body>
 </html>
--- htdocs/navtargets/Jokes Website/search_keyword.php
+++ htdocs/navtargets/Jokes Website/search_keyword.php
@@ -1,18 +1,24 @@
 <?php
+    //connect to the database
+    include "db.php";
 
-$keyword = "chicken";
+    //get the keyword parameter
+    $keyword = $_GET["keyword"];
 
-//get all jokes in table and display to webpage
-$sql = "SELECT JokeID, Joke_question, Joke_answer FROM Jokes WHERE Joke_question LIKE \"%chicken%\"";
-$result = $mysqli->query($sql);
+    //get all jokes in table and display to webpage
+    $sql = "SELECT JokeID, Joke_question, Joke_answer FROM Jokes WHERE Joke_question LIKE \"%".$keyword."%\"";
+    $result = $mysqli->query($sql);
 
-echo "<h2>Show all jokes with the word \"".$keyword."\"</h2>";
-if($result->num_rows > 0){
-    while($row = $result->fetch_assoc()){
-        echo "id: ".$row["JokeID"]." - Joke: ".$row["Joke_question"]." - Answer: ".$row["Joke_answer"]."<br>";
+    echo "<h2>Show all jokes with the word \"".$keyword."\"</h2>";
+    echo "<p><a href=\"/navtargets/Jokes%20Website\">Return to search page</a></p>";
+    if($result->num_rows > 0){
+        while($row = $result->fetch_assoc()){
+            echo "id: ".$row["JokeID"]." - Joke: ".$row["Joke_question"]." - Answer: ".$row["Joke_answer"]."<br>";
+        }
+    } else {
+        echo "0 results";
     }
-} else {
-    echo "0 results";
-}
 
+    //close the connection: we're done here
+    $mysqli->close();
 ?>
