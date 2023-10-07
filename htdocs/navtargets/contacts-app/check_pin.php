f589ee4ff737860bc76d8c9c58fc8b3506acaa23
AyOhEe <74308087+AyOhEe@users.noreply.github.com>
2023-10-07 15:28:24 +0100

    Added pin checking
    The pin is now compared to a text file that is VERY EXPLICITLY NOT in htdocs - if it were, you could see it by navigating to it in the URL bar.

--- htdocs/navtargets/contacts-app/check_pin.php
+++ htdocs/navtargets/contacts-app/check_pin.php
@@ -1,5 +1,12 @@
 <?php
     function checkPin($target_pin){
-        return $target_pin == "123456";
+        //open and read the file containing the pin
+        $path = "../../../contacts_pin.txt";
+        $pinfile = fopen($path, "r") or die("<h1>Unable to check pin!</h1>");
+        $pin = fread($pinfile, filesize($path));
+        fclose($pinfile);
+
+        //check if the given pin matches the actual pin
+        return $target_pin == $pin;
     }
 ?>
